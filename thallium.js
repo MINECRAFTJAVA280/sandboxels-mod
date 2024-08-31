6d50b4b96b98a35b9dad7f2c75f9790680f98864
MINECRAFTJAVA280 <Jadensopko765@outlook.com>
2024-08-31 10:00:03 -0700

    Add files via upload

---
+++ thallium.js
@@ -0,0 +1,27 @@
+elements.thallium = {
+	color: "#8bba86",
+	behavior: behaviors.sturdypowder,
+	category: "solids",
+	state: "solid",
+	tempHigh:  "579"
+    stateHigh: "Molten_thallium"
+    templow: "-76"
+    stateLow:thallium	
+	density: "100"
+	hidden: "false"
+	hardness: "1"
+	breakInto: "Thallium_oxide"
+	burn: "30"
+	burnTime: "75"
+	burnInto: "Thallium_oxide"
+	recations: { 
+     "water" { elem1: null,
+	 elem2: "thallium_oxide"
+	 elements.thallium_oxide = {
+	 color: "#526e54",
+	 behavior: behaviors.powder,
+	 category: "powders",
+	 state "solid",
+	 
+};
+
